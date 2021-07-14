b3dc41155a144a647947225bc6ffe149f23cad8d
EmmanuelToluwanimi <emmanueltoluwanimi01@gmail.com>
2021-07-15 00:35:15 +0100

    added material and

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './pages/Home';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+import { LinearProgress } from '@material-ui/core'
 import Nav from './components/Nav';
+
+const Home = lazy(() => import('./pages/Home'));
 
 function App() {
   return (
     <>
       <Router>
-        <Nav/>
-        <Switch>
-          <Route exact to="/" component={Home} />
-        </Switch>
+        <Suspense fallback={<LinearProgress variant="indeterminate" color="secondary" />}>
+          <Nav />
+          <Switch>
+            <Route exact to="/" component={Home} />
+          </Switch>
+        </Suspense>
       </Router>
     </>
   );
